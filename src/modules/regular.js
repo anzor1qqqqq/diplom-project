d72e0f07b7fe5b0ea5e34e7d99f82573326336e4
Anzor <anzor.shadzhe@gmail.com>
2023-03-16 17:47:42 +0300

    Регулярные выражения

---
+++ src/modules/regular.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const regularIputForm = () => {
+    const inputName = document.querySelector('[name="name"]');
+    const inputTel = document.querySelector('[name="telNum"]');
+
+    inputName.addEventListener('input', event => {
+        event.target.value = event.target.value.replace(/[^\а-яА-Я\s]/g, '');
+    });
+
+    inputTel.addEventListener('input', event => {
+        event.target.value = event.target.value.replace(/[^\d?\+]/g, '');
+    });
+};
+
+export default regularIputForm;
