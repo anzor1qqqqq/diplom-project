302e70ed0e7c9bed82df923acf295ea823f11565
Anzor <anzor.shadzhe@gmail.com>
2023-03-16 17:46:52 +0300

    Отправка формы

---
+++ src/modules/db-base.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const createDbBase = () => {
+    const formCallback = document.querySelector('[name="form-callback"]');
+    const spanState = document.querySelector('.span_state');
+
+    const awaitMessage = 'Идет отправка ваших данных...';
+    const succesMessage = 'Ваши данные отправлены';
+    const errorMessage = 'Произошла ошибка!';
+    
+    let bool;
+
+    const setDate = async (obj) => {
+        return await fetch('https://jsonplaceholder.typicode.com/todos', {
+            method: 'POST',
+
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            body: JSON.stringify(obj),
+        }).then((response) => response.json());
+    };
+    
+    formCallback.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const date = new FormData(formCallback);
+        const obj = {};
+
+        bool = true;
+
+        date.forEach((val, key) => {
+            if (val == '') bool = false;
+
+            obj[key] = val;
+        });
+
+        if (bool) {
+            spanState.textContent = awaitMessage;
+
+            setDate(obj).then((text) => {
+                console.log(text);
+
+                spanState.textContent = succesMessage;  
+            }).catch((error) => {
+                console.log(error);
+
+                spanState.style.color = 'red';
+                spanState.textContent = errorMessage; 
+            });
+
+            formCallback.reset();
+        };
+    });
+};
+
+export default createDbBase;
