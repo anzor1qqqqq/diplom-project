3013a1e13955bb6aac8c6e9a08cc237f288eedce
Anzor <anzor.shadzhe@gmail.com>
2023-03-17 21:23:27 +0300

    Скролл на главный экран

---
+++ src/modules/scroll-arrow.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const scrollArrow = () => {
+    const upArrow = document.querySelector('.up');
+    const servicesSection = document.querySelector('.services-section ');
+
+    document.addEventListener('scroll', () => {
+        let coordinateY = servicesSection.getBoundingClientRect().y;
+
+        if (coordinateY <= 0) {
+            upArrow.style.display = 'block';
+        } else if (coordinateY >= 300) {
+            upArrow.style.display = 'none';
+        };
+    });
+
+    upArrow.addEventListener('click', event => {
+        event.preventDefault();
+
+        const id = upArrow.getAttribute('href');
+        console.log(document.querySelector(id));
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+};
+
+export default scrollArrow;
