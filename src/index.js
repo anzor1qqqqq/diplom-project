f450634f7c94505a62602914ac45540d9d8af83e
Anzor <anzor.shadzhe@gmail.com>
2023-03-17 20:36:41 +0300

    modified:   dist/css/style.css 	modified:   dist/script/main.js 	modified:   index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import menuNavigator from "./modules/navigator";
 import createModalWindow from "./modules/modal";
 import createDbBase from "./modules/db-base";
 import regularIputForm from "./modules/regular";
+import sliderPreview from "./modules/slider";
 
 menuNavigator();
 createModalWindow();
 createDbBase();
 regularIputForm();
+sliderPreview();
